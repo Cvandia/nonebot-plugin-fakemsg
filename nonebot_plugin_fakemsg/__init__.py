6433d7be84164b6ba0934fd12337e14916a2536f
divandia <106718176+Cvandia@users.noreply.github.com>
2023-08-31 09:09:20 +0800

    update: 更新`@群友`触发伪造消息
    详细更新如下:
    - 更新完善PluginMetadata
    - 替换on_regex改为on_message
    - 更换逻辑，使用CQ码解析(可能有bug？

--- nonebot_plugin_fakemsg/__init__.py
+++ nonebot_plugin_fakemsg/__init__.py
@@ -7,8 +7,7 @@ from nonebot.adapters.onebot.v11 import (
 )
 import re
 from typing import List, Union
-from nonebot.plugin import on_regex, PluginMetadata
-from nonebot.params import RegexStr
+from nonebot.plugin import on_message, PluginMetadata
 from .config import config
 
 __plugin_meta__ = PluginMetadata(
@@ -23,7 +22,10 @@ __plugin_meta__ = PluginMetadata(
         "menu_data": [
             {
                 "func": "伪造消息",
-                "trigger_method": "on_regex",
+                "trigger_method": "on_message",
+                "trigger_condition": "暂无介绍",
+                "brief_des": "用于伪造恶搞群友或者好友的消息",
+                "detail_des": "可使用的命令：\nqq+说+内容|qq+说+内容\n\n例如：\n123456说你好|654321说你好\n\n注意：\n1. 伪造消息的qq号必须是机器人好友或者在群内\n2. 伪造消息的qq号必须是数字\n3. 伪造消息的qq号必须是6-10位\n4. 伪造消息的内容不能包含|和说\n5. 伪造消息的内容不能超过30个字符\n6. 伪造消息的内容不能包含特殊字符\n7. 伪造消息的内容不能包含CQ码\n8. 伪造消息的内容不能包含空格\n9. 伪造消息的内容不能包含换行符\n10. 伪造消息的内容不能包含回车符\n11. 伪造消息的内容不能包含@",
             }
         ],
         "menu_template": "default",
@@ -33,35 +35,49 @@ __plugin_meta__ = PluginMetadata(
 user_split = config.user_split
 message_split = config.message_split
 
-send_fake_msg = on_regex(r"^(\d{6,10})说(.*)", flags=re.I, priority=5, block=False)
+
+async def check_if_fakemsg(bot: Bot, event: MessageEvent) -> bool:
+    """
+    检查是否为伪造消息
+
+    > 参数：
+        - bot: Bot 对象
+        - event: MessageEvent 对象
+
+    > 返回值：
+        - 是伪造消息：True
+        - 不是伪造消息：False
+    """
+    if event.original_message[0].type == "at":
+        if event.original_message[1].data["text"].strip().startswith("说"):
+            return True
+    elif event.original_message[0].type == "text":
+        if re.match(r"^\d{6,10}说", event.original_message[0].data["text"]):
+            return True
+    return False
+
+
+send_fake_msg = on_message(rule=check_if_fakemsg, priority=5, block=True)
 
 
 @send_fake_msg.handle()
-async def _(
-    bot: Bot,
-    event: Union[PrivateMessageEvent, GroupMessageEvent],
-    args: str = RegexStr(),
-):
-    await send_fake_msg.send(f"正在伪造消息...")
+async def _(bot: Bot, event: Union[PrivateMessageEvent, GroupMessageEvent]):
+    await send_fake_msg.send("正在伪造消息……")
+    raw_message = event.raw_message
     fake_msg_list = []
-    users_msgs = args.split(user_split)
-    for user_msg in users_msgs:
-        try:
-            qq = user_msg.split("说", 1)[0]  # 以 ‘说’ 为分隔qq号和消息内容
-            msgs = user_msg.split("说", 1)[1].split(message_split)  # 以空格为分隔消息内容和消息内容
-        except IndexError:
-            await send_fake_msg.finish("消息格式错误")
-        try:
-            res = await bot.get_stranger_info(user_id=qq)
-        except Exception as e:
-            await send_fake_msg.finish(f"获取用户信息失败{e},请检查qq号是否正确")
-        name = res["nickname"]
+    user_msgs = raw_message.split(user_split)
+    for user_msg in user_msgs:
+        qq_msg = user_msg.split("说", 1)[0].strip()
+        if qq_msg.isdigit():
+            qq = qq_msg
+        else:
+            qq = re.match(r"^\[CQ:at,qq=(\d{6,10})\]", qq_msg).group(1)
+        msgs = user_msg.split("说", 1)[1].split(message_split)
+        name = await bot.get_stranger_info(user_id=qq)
+        name = name["nickname"]
         for msg in msgs:
-            fake_msg_list.append((name, qq, msg))
-    try:
-        await send_forward_msg(bot, event, fake_msg_list)
-    except Exception as e:
-        await send_fake_msg.finish(f"发送失败{e}")
+            fake_msg_list.append((name, qq, Message(msg)))
+    await send_forward_msg(bot, event, fake_msg_list)
 
 
 async def send_forward_msg(
