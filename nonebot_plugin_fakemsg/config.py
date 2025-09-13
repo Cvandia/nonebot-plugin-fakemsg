655e979c8d5e8b2029639c7df6cbf8141d6b3873
Cvandia <106718176+Cvandia@users.noreply.github.com>
2025-09-13 10:54:57 +0800

    ⚡️ 更新版本号并发布白名单功能

--- nonebot_plugin_fakemsg/config.py
+++ nonebot_plugin_fakemsg/config.py
@@ -1,5 +1,3 @@
-from typing import List
-
 from nonebot.plugin import get_plugin_config
 from pydantic import BaseModel
 
@@ -7,7 +5,7 @@ from pydantic import BaseModel
 class Config(BaseModel):
     user_split: str = "|"
     message_split: str = " "
-    fakemsg_whitelist: List[str] = []
+    fakemsg_whitelist: list[str] = []
 
 
 config = get_plugin_config(Config)
